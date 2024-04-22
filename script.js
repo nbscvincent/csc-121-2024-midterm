4c3b54967062e8c858dc75f4431d286aa5773192
Rhod Lenard Delas Nieves <rdelasnieves2023@student.nbscollege.edu.ph>
2024-04-22 15:32:10 +0800

    Initial commit

--- script.js
+++ script.js
@@ -4,3 +4,42 @@ function validateUser(){
   window.location.href = "homescreen.html";
   console.log(log_email.value + "\n" + log_pass.value)
 }
+
+var tasks = [];
+
+    function addTask() {
+        var taskInput = document.getElementById("taskInput");
+        var task = taskInput.value.trim();
+        
+        if (task !== "") {
+            tasks.push(task);
+            displayTasks();
+            taskInput.value = "";
+        } else {
+            alert("Please enter a task.");
+        }
+    }
+
+    function deleteTask(index) {
+        tasks.splice(index, 1);
+        displayTasks();
+    }
+
+    function displayTasks() {
+        var taskList = document.getElementById("taskList");
+        taskList.innerHTML = "";
+
+        tasks.forEach(function(task, index) {
+            var li = document.createElement("li");
+            li.textContent = task;
+
+            var deleteBtn = document.createElement("button");
+            deleteBtn.textContent = "Delete";
+            deleteBtn.onclick = function() {
+                deleteTask(index);
+            };
+
+            li.appendChild(deleteBtn);
+            taskList.appendChild(li);
+        });
+    }
